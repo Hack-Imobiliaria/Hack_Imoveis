7125fb44f86d852656149247bff5b21e536af17f
Wagner Rodrigo da Silva <wagnerrodrigo.pan@gmail.com>
2018-05-31 14:58:24 -0300

    metodos

--- Hack_Imoveis/src/Model/Venda.java
+++ Hack_Imoveis/src/Model/Venda.java
@@ -6,13 +6,35 @@
 package Model;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 /**
  *
  * @author wagner
  */
 public class Venda {
+    private static int autoIncremento =0;
     private Cliente cliente;
     private float valorVenda;
     private Date dataVenda;
+    private List<Aluguel> lista;
+    private List<Venda> listaVenda;
+    
+    
+public Venda(){
+    autoIncremento++;
+    lista = new ArrayList<Aluguel>();
+    listaVenda = new ArrayList<Venda>();
+    dataVenda = new Date();
+}
+    public void inserirAluguel(Aluguel aluguel)
+    {
+        lista.add(aluguel);
+    }
+    
+    public void inserirVenda(Venda venda)
+    {   
+        listaVenda.add(venda);
+    }
+    
     
 }
