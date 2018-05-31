a95233ff3558f89ce44a178dcb61b48e0b5d30fd
Wagner Rodrigo da Silva <wagnerrodrigo.pan@gmail.com>
2018-05-31 15:15:13 -0300

    classe endereço

---
+++ Hack_Imoveis/src/Model/Endereco.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author wagner
+ */
+public class Endereco {
+    private String logradouro;
+    private String complemento;
+    private String cidade;
+    private String estado;
+    private String cep;
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public Endereco(String logradouro, String complemento, String cidade, String estado, String cep) {
+        this.logradouro = logradouro;
+        this.complemento = complemento;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.cep = cep;
+    }
+    
+}
