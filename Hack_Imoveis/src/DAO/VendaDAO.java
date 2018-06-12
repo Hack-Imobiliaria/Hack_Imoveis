4a21f2dd106ac98ed354116117dbc30cae99050f
Denner_Rondinely <denner.rondinely@gmail.com>
2018-06-11 23:48:09 -0300

    vendasDAO

--- Hack_Imoveis/src/DAO/VendaDAO.java
+++ Hack_Imoveis/src/DAO/VendaDAO.java
@@ -5,10 +5,58 @@
  */
 package DAO;
 
+import Model.Venda;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Denner
  */
 public class VendaDAO {
+    private List<Venda> lista = null;
+
+    public VendaDAO() {
+        lista = new ArrayList<Venda>();
+    }
+    public void inserirVenda(Venda venda)
+    {
+        lista.add(venda);
+    }
     
+    public Venda buscarVenda(int numero)
+    {
+        Venda v = null;
+        for(Venda venda: lista)
+        {
+            if(venda.getNumero() == numero)
+                v = venda;
+        }
+        
+        return v;
+    }
+    
+    public void removerVenda(int numero)
+    {
+        Venda venda = buscarVenda(numero);
+        if(venda != null)
+        {
+            lista.remove(venda);
+        }
+    }
+    
+    public void alterarVenda(int numero)
+    {
+        Venda venda = buscarVenda(numero);
+        if(venda != null)
+        {
+            int pos = lista.indexOf(venda);
+            lista.set(pos, venda);
+        }
+    }
+    
+    public List<Venda> todosVendas()
+    {
+        return lista;
+    }
 }
