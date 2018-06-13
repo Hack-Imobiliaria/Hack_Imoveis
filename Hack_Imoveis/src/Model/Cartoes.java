ec97cff966458427f5d8bee2c1ffea3a453a3cc2
Wagner Rodrigo da Silva <wagnerrodrigo.pan@gmail.com>
2018-06-13 18:09:43 -0300

    classe cartao

---
+++ Hack_Imoveis/src/Model/Cartoes.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author wagner
+ */
+public class Cartoes {
+    private String nomeTitular ;
+    private int numeroCartao;
+    private Date dataValidade;
+    private int codigoVerificacao;
+
+    // construtor
+
+    public Cartoes(String nomeTitular, int numeroCartao, Date dataValidade, int codigoVerificacao) {
+        this.nomeTitular = nomeTitular;
+        this.numeroCartao = numeroCartao;
+        this.dataValidade = dataValidade;
+        this.codigoVerificacao = codigoVerificacao;
+    }
+    
+    
+    
+    // metodo get e set 
+    public String getNomeTitular() {
+        return nomeTitular;
+    }
+
+    public void setNomeTitular(String nomeTitular) {
+        this.nomeTitular = nomeTitular;
+    }
+
+    public int getNumeroCartao() {
+        return numeroCartao;
+    }
+
+    public void setNumeroCartao(int numeroCartao) {
+        this.numeroCartao = numeroCartao;
+    }
+
+    public Date getDataValidade() {
+        return dataValidade;
+    }
+
+    public void setDataValidade(Date dataValidade) {
+        this.dataValidade = dataValidade;
+    }
+
+    public int getCodigoVerificacao() {
+        return codigoVerificacao;
+    }
+
+    public void setCodigoVerificacao(int codigoVerificacao) {
+        this.codigoVerificacao = codigoVerificacao;
+    }
+    
+    
+}
